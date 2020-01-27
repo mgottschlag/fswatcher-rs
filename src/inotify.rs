2019f37ee92f038b5708eb24b3515f2fb02d7249
Mathias Gottschlag <mgottschlag@gmail.com>
2020-01-27 23:19:16 +0100

    Reworked inotify event processing logic.
    We must not add watches before the whole inotify stream buffer has been
    drained. Otherwise, inotify might reuse the watch descriptor and we might
    misattribute events.

--- src/inotify.rs
+++ src/inotify.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 use std::ffi::{OsStr, OsString};
 use std::fs;
 use std::path::Path;
@@ -14,10 +14,10 @@ pub struct FileSystemWatcherInotify {
     root_dir: OsString,
     inotify: Inotify,
     stream: Pin<Box<EventStream<InotifyBuffer>>>,
-    new_directories: VecDeque<OsString>,
+    new_directories: BTreeSet<OsString>,
     watches_by_path: BTreeMap<OsString, WatchDescriptor>,
     paths_by_watch: HashMap<WatchDescriptor, OsString>,
-    removed_watches: HashSet<WatchDescriptor>,
+    //removed_watches: HashSet<WatchDescriptor>,
 }
 
 impl FileSystemWatcherInotify {
@@ -25,32 +25,28 @@ impl FileSystemWatcherInotify {
         let mut inotify = Inotify::init()?;
         let stream = inotify.event_stream(InotifyBuffer { data: [0; 1024] })?;
 
+        let mut new_directories = BTreeSet::new();
+        new_directories.insert(path.to_owned());
         Ok(FileSystemWatcherInotify {
             root_dir: path.to_owned(),
             inotify,
             stream: Box::pin(stream),
-            new_directories: vec![path.to_owned()].into(),
+            new_directories,
             watches_by_path: BTreeMap::new(),
             paths_by_watch: HashMap::new(),
-            removed_watches: HashSet::new(),
         })
     }
 
     fn translate_inotify_event(&mut self, inotify_event: EventOwned) -> Option<FileSystemEvent> {
-        // Ignore events for watches which are not used anymore but have not yet been removed. This
-        // situation can happen if we remove a watch but there are still events buffered by the
-        // inotify crate.
-        if self.removed_watches.contains(&inotify_event.wd) {
-            if inotify_event.mask == EventMask::IGNORED {
-                // We need to remove the watch from the list, otherwise really_delete_watches()
-                // will fail.
-                self.removed_watches.remove(&inotify_event.wd);
-            }
+        // TODO: Modify code to delete entries from new_directories as well.
+
+        if inotify_event.mask == EventMask::IGNORED {
+            // We manually deleted the watch or the directory was deleted. In any case, there is
+            // nothing to do here.
             return None;
         }
         if !self.paths_by_watch.contains_key(&inotify_event.wd) {
-            // This should never happen.
-            eprintln!("Huh, event for unknown inotify watch received?");
+            // We probably already deleted the watch. Ignore the event.
             return None;
         }
 
@@ -69,7 +65,6 @@ impl FileSystemWatcherInotify {
         // and I/O load caused by deleting and reestablishing all the watches for the
         // subdirectories of a moved directory, but the code would become considerably more
         // complex.
-        println!("{}, {:?}", path.to_string_lossy(), inotify_event);
         if inotify_event.mask == EventMask::CREATE && name_available {
             Some(FileSystemEvent::FileCreated(path))
         } else if inotify_event.mask == EventMask::MODIFY && name_available {
@@ -92,7 +87,7 @@ impl FileSystemWatcherInotify {
             // We do not generate events for existing contents of the directory - the caller just
             // is notified that we started monitoring the directory and has to detect changes
             // themselves. The same logic is already required during initialization.
-            self.new_directories.push_back(path.clone());
+            self.new_directories.insert(path.clone());
             Some(FileSystemEvent::DirectoryCreated(path))
         } else if inotify_event.mask == EventMask::DELETE | EventMask::ISDIR && name_available {
             self.delete_watches(&path);
@@ -107,7 +102,7 @@ impl FileSystemWatcherInotify {
             // We do not generate events for existing contents of the directory - the caller just
             // is notified that we started monitoring the directory and has to detect changes
             // themselves. The same logic is already required during initialization.
-            self.new_directories.push_back(path.clone());
+            self.new_directories.insert(path.clone());
             // TODO: Store the move cookie in the event so that it can later be combined with the
             // MOVED_TO event.
             Some(FileSystemEvent::DirectoryCreated(path))
@@ -121,22 +116,12 @@ impl FileSystemWatcherInotify {
             } else {
                 None
             }
-        } else if inotify_event.mask == EventMask::IGNORED {
-            // We already received an event about the directory being deleted - we should never get
-            // this event here.
-            eprintln!(
-                "IGNORED received ({}), but no directory removal event!",
-                path.to_string_lossy()
-            );
-            // TODO: How to handle this situation?
-            None
         } else {
             eprintln!(
                 "Warning: Unexpected inotify event: {}, {:?}",
                 directory.to_string_lossy(),
                 inotify_event
             );
-            // TODO
             None
         }
     }
@@ -152,7 +137,7 @@ impl FileSystemWatcherInotify {
                                     if file_type.is_dir() {
                                         let new_dir =
                                             entry.path().as_path().as_os_str().to_os_string();
-                                        self.new_directories.push_back(new_dir);
+                                        self.new_directories.insert(new_dir);
                                     }
                                 }
                                 Err(e) => eprintln!(
@@ -183,10 +168,7 @@ impl FileSystemWatcherInotify {
     }
 
     fn delete_watches(&mut self, path: &OsStr) {
-        // Move watches for the directory and for all subdirectories to the list of watches to be
-        // deleted when the buffer has been cleared. We do not immediately delete the watches as
-        // there  might still be events in the buffer of the inotify crate, so there might be some
-        // races where a new watch reuses a watch ID and events are misattributed.
+        // Remove watches for the directory and for all subdirectories.
         let mut watches_to_delete = Vec::new();
         let mut path_iter = self.watches_by_path.range(path.to_owned()..);
         loop {
@@ -205,28 +187,62 @@ impl FileSystemWatcherInotify {
             }
         }
         for (p, wd) in watches_to_delete.into_iter() {
-            println!("Will delete {}/{:?}", p.to_string_lossy(), wd);
             self.watches_by_path.remove(&p);
             self.paths_by_watch.remove(&wd);
-            self.removed_watches.insert(wd);
+            // We ignore errors here, because the IGNORED event for the watch might already be in
+            // the inotify buffer (meaning that the watch is already invalid), we just did not read
+            // and process it yet.
+            self.inotify.rm_watch(wd).ok();
         }
-        // We do not have to delete the entry from new_directories, as new watches are installed
-        // before the next time inotify events are fetched, so at this point there cannot be
-        // any entry in new_directories.
+        // We also have to delete the entries from new_directories, as watches are only added once
+        // the inotify buffer has been drained. We do not want to accidently add watches for these
+        // deleted directories if the user concurrently creates a new directory.
+        //
+        // This has to be as complicated as is, because new_directories might contain an arbitrary
+        // number of subdirectories of an (already watched) directory.
+        let mut new_dirs_to_delete = Vec::new();
+        let mut path_iter = self.new_directories.range(path.to_owned()..);
+        loop {
+            match path_iter.next() {
+                None => break,
+                Some(p) => {
+                    let p_path: &Path = p.as_ref();
+                    if p_path.starts_with(path) {
+                        new_dirs_to_delete.push(p.clone());
+                    } else {
+                        // The paths are sorted, so the first entry that does not start with the
+                        // path passed to this function limits the subdirectories.
+                        break;
+                    }
+                }
+            }
+        }
+        for p in new_dirs_to_delete.into_iter() {
+            self.new_directories.remove(&p);
+        }
     }
 
-    fn really_delete_watches(&mut self) {
-        // Note that most watches are probably deleted at the top of translate_inotify_event()
-        // already - however, if the directory was just moved, some watches might still be valid.
-        // TODO: Check whether this is racy, and what happens then directories are concurrently
-        // deleted.
-        for wd in self.removed_watches.iter().cloned() {
-            println!("rm_watch {:?}", wd);
-            self.inotify
-                .rm_watch(wd)
-                .expect("could not remove inotify watch");
+    fn poll_inotify_stream(&mut self, cx: &mut Context) -> Poll<Option<FileSystemEvent>> {
+        loop {
+            match Pin::as_mut(&mut self.stream).poll_next(cx) {
+                Poll::Pending => {
+                    return Poll::Pending;
+                }
+                Poll::Ready(None) => return Poll::Ready(None),
+                Poll::Ready(Some(Ok(event))) => {
+                    let translated = self.translate_inotify_event(event);
+                    if let Some(event) = translated {
+                        return Poll::Ready(Some(event));
+                    } else {
+                        // Some inotify events do not directly translate into our events, such
+                        // as MOVED_FROM/MOVED_TO. Simply try to read the next event.
+                    }
+                }
+                Poll::Ready(Some(Err(e))) => {
+                    return Poll::Ready(Some(FileSystemEvent::Error(e.into())))
+                }
+            };
         }
-        self.removed_watches.clear();
     }
 }
 
@@ -237,34 +253,22 @@ impl Stream for FileSystemWatcherInotify {
         // Safe, as we will not move self_.
         let self_ = unsafe { self.get_unchecked_mut() };
 
+        // Here, the order is important to prevent watch descriptor reuse. We must first drain
+        // the inotify buffer before we can add any new watches. See
+        // https://github.com/hannobraun/inotify/issues/73 for a description of the issue.
+
+        match self_.poll_inotify_stream(cx) {
+            Poll::Pending => {
+                // Continue below and add any
+            }
+            x => return x,
+        }
+
         loop {
-            if self_.new_directories.is_empty() {
-                // If all the inotify watches are up-to-date, simply wait for any events from inotify.
-                match Pin::as_mut(&mut self_.stream).poll_next(cx) {
-                    Poll::Pending => {
-                        // The buffer has been cleared, so no we can delete watches without having
-                        // to fear race conditions due to the reuse of watch IDs.
-                        self_.really_delete_watches();
-                        return Poll::Pending;
-                    }
-                    Poll::Ready(None) => return Poll::Ready(None),
-                    Poll::Ready(Some(Ok(event))) => {
-                        let translated = self_.translate_inotify_event(event);
-                        if let Some(event) = translated {
-                            return Poll::Ready(Some(event));
-                        } else {
-                            // Some inotify events do not directly translate into our events, such
-                            // as MOVED_FROM/MOVED_TO. Simply try to read the next event.
-                        }
-                    }
-                    Poll::Ready(Some(Err(e))) => {
-                        return Poll::Ready(Some(FileSystemEvent::Error(e.into())))
-                    }
-                };
-            } else {
-                // Install an inotify watch for the directory and report that the directory was added.
-                let new_directory = self_.new_directories.pop_front().unwrap();
-                // TODO: Check whether the list of active watches already contains the directory.
+            if !self_.new_directories.is_empty() {
+                // Install an inotify watch for a new directory and report that the directory was added.
+                let new_directory = self_.new_directories.iter().next().unwrap().clone();
+                self_.new_directories.remove(&new_directory);
 
                 // TODO: Do not follow links!
                 // TODO: Is ONLYDIR correct?
@@ -297,9 +301,13 @@ impl Stream for FileSystemWatcherInotify {
 
                     return Poll::Ready(Some(FileSystemEvent::DirectoryWatched(new_directory)));
                 } else {
-                    // Just try again.
+                    // We failed, but there might be more new directories. Just try again.
+                    continue;
                 }
             }
+
+            // No events, no new directories.
+            return Poll::Pending;
         }
     }
 }
